707b64344fc54c9b0099c4ae5cdcc48d817395ef
Caroline kahuro <carolkahuro@gmail.com>
2023-07-05 09:40:31 +0300

    done assignment

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance) {
+  if (distance <=199) {return 'This one is on me!'}
+  else if(distance ===2001) {return 'I will gladly take your thirty bucks.'}
+  else if(distance === 2501) {return 'No can do.'}
+}
+  
+function ternaryCheckCity(City){
   // Write your code here!
+  return City === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    case 'thanks for everything':
+      return "Bye.";
 
-function switchOnCharmFromTip(){
+  }
   // Write your code here!
 }
